0ab565627403ce9fb1b5d7db8face654f11935e9
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-22 19:23:23 +0200

    chore: update BitcoinExchange class definition and clean up includes

--- ex00/include/BitcoinExchange.hpp
+++ ex00/include/BitcoinExchange.hpp
@@ -3,34 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 18:53:53 by descamil          #+#    #+#             */
-/*   Updated: 2025/06/18 19:37:09 by descamil         ###   ########.fr       */
+/*   Updated: 2025/06/21 20:26:32 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <ctime>
+# include <cmath>
+# include <limits>
 # include <string>
+# include <vector>
+# include <cstdlib>
+# include <fstream>
+# include <sstream>
+# include <utility>
 # include <iostream>
-# include <fstream> 
 # include <algorithm>
+# include <stdexcept>
 
-class btc
+class BitcoinExchange
 {
-    private:
-        const std::string   _database;
-        const std::string   _filename;
-    public:
-        btc();
-        btc(std::string filename);
-        ~btc();
-        btc(const btc &copy);
-        btc& operator=(const btc &copy);
-        
-        std::string line;
+	private:
+		std::pair<std::string, std::string> database;
+	public:
+		BitcoinExchange();
+		~BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& other);
+		BitcoinExchange& operator=(const BitcoinExchange& other);
+
+		void	fillDatabase(const std::string& file);
 };
 
 #endif // BITCOINEXCHANGE_HPP
