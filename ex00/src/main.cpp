65d7e000786d339deed06bd31ceeb67c130d7284
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-22 19:23:32 +0200

    chore: update main function to improve argument validation and clean up code

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,38 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 18:54:06 by descamil          #+#    #+#             */
-/*   Updated: 2025/06/18 19:38:37 by descamil         ###   ########.fr       */
+/*   Updated: 2025/06/21 19:59:40 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/BitcoinExchange.hpp"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argc != 2)
     {
-        std::cerr << "Error: could not open file." << std::endl;
+        std::cerr << "Error: invalid number of arguments" << std::endl;
         return 1;
     }
-    std::ifstream inputFile(argv[1]);
-    if (inputFile.is_open())
-    {
-        btc input;
-        while (std::getline(inputFile, input.line))
-        {
-            if (std::find(input.line.begin(), input.line.end(), '|') != input.line.end())
-                (void)input; // Guardar la linea
-            else
-                (void)input; // Salir con error especifico
-        }
-    }
-    else
-    {
-        std::cerr << "Error: could not open file." << std::endl;
-        return 1;
-    }
-    
+
+	BitcoinExchange btc;
+	std::vector<std::pair<std::string, std::string> > database;
+	std::vector<std::pair<std::string, std::string> > infile;
+
+	return 0;
 }
