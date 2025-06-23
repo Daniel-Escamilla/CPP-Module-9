325b515a05c30b7b674802cf3c7e0f6d5bc0b830
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-23 18:58:08 +0200

    feat: add RPN header file with token structure and function declarations

---
+++ ex01/include/RPN.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/23 14:30:34 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/06/23 18:55:07 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <math.h>
+# include <string>
+# include <limits>
+# include <vector>
+# include <sstream>
+# include <iostream>
+
+struct Token
+{
+	enum Type { Number, Operator } type;
+	int number;
+	char oper;
+};
+
+std::vector<Token>	extractTokens(const char* expr);
+void				processTokens(std::vector<Token> tokens);
+
+
+#endif // RPN_HPP
