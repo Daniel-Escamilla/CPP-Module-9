60d6527eee1da2e653aacc0119adf7bc4c80bf08
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-23 14:15:10 +0200

    chore: update timestamp in header comment and clean up main function structure

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 18:54:06 by descamil          #+#    #+#             */
-/*   Updated: 2025/06/21 19:59:40 by daniel-esca      ###   ########.fr       */
+/*   Updated: 2025/06/23 11:35:51 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,15 @@ int main(int argc, char** argv)
         std::cerr << "Error: invalid number of arguments" << std::endl;
         return 1;
     }
-
-	BitcoinExchange btc;
-	std::vector<std::pair<std::string, std::string> > database;
-	std::vector<std::pair<std::string, std::string> > infile;
+    try 
+    {
+        BitcoinExchange btc;
+        btc.loadAndCompare(btc, argv[1]);
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 
 	return 0;
 }
