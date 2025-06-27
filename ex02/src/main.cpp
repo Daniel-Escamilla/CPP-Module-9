156e774c317692c6e0bb6b5e71b7099989441848
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-27 18:12:34 +0200

    feat: implement main function for PmergeMe number processing

---
+++ ex02/src/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 12:07:52 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/06/26 12:28:19 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/PmergeMe.hpp"
+
+int main(int argc, char **argv)
+{
+	try
+	{
+		if (argc != 2)
+			throw std::invalid_argument("Incorrect number of arguments");
+		if (std::string(argv[1]).find_first_not_of(' ') == std::string::npos
+			|| std::string(argv[1]).find_first_not_of('\n') == std::string::npos
+			|| std::string(argv[1]).find_first_not_of('\t') == std::string::npos)
+			throw std::invalid_argument("Argument Empty");
+		PmergeMe list;
+		list.readNumbers(std::string(argv[1]));
+		
+		list.printTest();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
