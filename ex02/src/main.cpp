7896a2b01ca03f9a43c74305f273cc70dfca5888
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-30 10:38:33 +0200

    fix: update main function to remove unnecessary blank line

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:07:52 by daniel-esca       #+#    #+#             */
-/*   Updated: 2025/06/26 12:28:19 by daniel-esca      ###   ########.fr       */
+/*   Updated: 2025/06/28 08:04:48 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 			throw std::invalid_argument("Argument Empty");
 		PmergeMe list;
 		list.readNumbers(std::string(argv[1]));
-		
+		list.insertNumbers();
 		list.printTest();
 	}
 	catch (const std::exception &e)
